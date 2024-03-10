adfae59cbf62c5219e1ec8bac1db04b1e267b9f1
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2024-03-10 21:39:50 +0200

    fix user creation migration

--- migrations/20240310181608-create-user.js
+++ migrations/20240310181608-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,37 +10,49 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(80),
+        field: 'first_name',
+        allowNull: false
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(80),
+        field: 'last_name',
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
       },
       password: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       isMale: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        field: 'is_male'
       },
       birthdey: {
         type: Sequelize.DATEONLY
       },
       balance: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(9,2),
+        allowNull: false,
+        defaultValue: 0
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'creared_at'
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'updated_at'
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
